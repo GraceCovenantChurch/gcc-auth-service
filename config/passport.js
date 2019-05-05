75560af2f165b08baa79a919adfaf8b21655d630
joeyzia <joey.zia@gmail.com>
2019-05-05 09:57:53 -0400

    Minor Change
    Updating Spacing

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,14 @@
 var passport = require("passport");
 var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
+
 passport.deserializeUser(function(user, done) {
   done(null, user);
 });
+
 passport.use(
   new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
